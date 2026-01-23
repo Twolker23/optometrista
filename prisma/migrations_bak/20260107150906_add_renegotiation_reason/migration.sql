fb27b7fcd8392e4d5c391dc9ed6e24e7daf0968c
Twolker23 <lucasm315@gmail.com>
2026-01-23 12:45:57 -0300

    chore: remove prisma migrations_bak

--- prisma/migrations_bak/20260107150906_add_renegotiation_reason/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "RenegotiationReason" AS ENUM ('NAO_COMPARECEU', 'NAO_COMPROU');
-
--- AlterTable
-ALTER TABLE "Client" ADD COLUMN "renegotiationReason" "RenegotiationReason";
